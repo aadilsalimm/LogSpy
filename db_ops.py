c4abbc6f52a399a3e7f33271865322be89388169
Aadil <aadilsalimm@gmail.com>
2026-03-14 00:06:05 +0530

    Added an autoincrement id as db table's primary key.

--- db_ops.py
+++ db_ops.py
@@ -9,7 +9,8 @@ def connect_db():
 
 def add_data(data):
     cur = db_connection.cursor()
-    query = f'''INSERT INTO history VALUES(
+    query = f'''INSERT INTO history (timestamp, is_anomalous, component, description)
+                VALUES(
                 '{data['timestamp']}',
                 {data['is_anomalous']},
                 '{data['component']}',
@@ -24,9 +25,9 @@ def clear_data():
     db_connection.commit()
 
 
-def delete_data(timestamp):
+def delete_data(id):
     cur = db_connection.cursor()
-    cur.execute(f'''DELETE FROM history WHERE timestamp = '{timestamp}' ''')
+    cur.execute(f'''DELETE FROM history WHERE id = {id} ''')
     db_connection.commit()
 
 
@@ -37,6 +38,7 @@ def get_all_data():
 
     return [
         {
+            "id": int(row["id"]),
             "timestamp": row["timestamp"],
             "is_anomalous": bool(row["is_anomalous"]),
             "component": row["component"],
@@ -54,14 +56,14 @@ def close_db_connection():
 
 
 if __name__ == "__main__":
-    # import json
-    # raw_data = '''{"is_anomalous": 0, "timestamp": "2026-03-13 19:15:27", "reason": "The logs show normal user authentication and authorization activities, including sudo commands and lockdown restrictions.", "component": "Authentication/Authorization"}'''
-    # data = json.loads(raw_data)
-    # connect_db()
-    # add_data(data)
+    import json
+    raw_data = '''{"is_anomalous": 0, "timestamp": "2026-03-13 19:15:27", "reason": "The logs show normal user authentication and authorization activities, including sudo commands and lockdown restrictions.", "component": "Authentication/Authorization"}'''
+    data = json.loads(raw_data)
+    connect_db()
+    add_data(data)
     # # clear_data()
     # connect_db()
     # delete_data("2026-03-13 19:15:27")
-    connect_db()
+    # connect_db()
     print(get_all_data())
     close_db_connection()
